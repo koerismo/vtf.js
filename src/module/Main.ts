d17c96ac8dc2cb169ff4e10415655ec06e1631aa
Koerismo <56563658+koerismo@users.noreply.github.com>
2022-07-23 20:20:42 -0400

    Prep for new version

--- src/module/Main.ts
+++
@@ -1,8 +0,0 @@
-import { Vtf, Frame, VtfImageResource } from "./VtfContainer.js";
-import { VtfEncodings } from "./VtfEncodings.js";
-import { VtfDxtEncodings } from "./VtfDxtEncodings.js";
-
-Vtf.registerCodec( VtfEncodings.RGB888 );
-Vtf.registerCodec( VtfEncodings.RGBA8888 );
-Vtf.registerCodec( VtfDxtEncodings.DXT1 );
-Vtf.registerCodec( VtfDxtEncodings.DXT5 );
